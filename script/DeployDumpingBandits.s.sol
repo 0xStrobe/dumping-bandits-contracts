9c2d5b50f7a2535d5a93f98b2bb6f3d4f3530821
strobie <0xstrobe@protonmail.com>
2023-02-16 23:27:36 +0100

    update deployment script

--- script/DeployDumpingBandits.s.sol
+++ script/DeployDumpingBandits.s.sol
@@ -4,9 +4,11 @@ pragma solidity ^0.8.18;
 import "forge-std/Script.sol";
 import {RandomnessClient} from "../src/RandomnessClient.sol";
 import {DumpingBandits} from "../src/DumpingBandits.sol";
+import {BanditTreasury} from "../src/BanditTreasury.sol";
 
 contract DeployDumpingBandits is Script {
     RandomnessClient public rc;
+    BanditTreasury public treasury;
     DumpingBandits public dumpingBandits;
 
     function run() public {
@@ -15,7 +17,10 @@ contract DeployDumpingBandits is Script {
 
         rc = new RandomnessClient();
         console.log("Deployed RandomnessClient at", address(rc));
-        dumpingBandits = new DumpingBandits(address(rc));
+        treasury = new BanditTreasury();
+        console.log("Deployed BanditTreasury at", address(treasury));
+
+        dumpingBandits = new DumpingBandits(address(rc), address(treasury));
         console.log("Deployed DumpingBandits at", address(dumpingBandits));
 
         vm.stopBroadcast();
