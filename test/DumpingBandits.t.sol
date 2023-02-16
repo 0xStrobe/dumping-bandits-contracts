846efd1c489bab753e09a0fe6831432cec51473d
strobie <0xstrobe@protonmail.com>
2023-02-16 23:30:31 +0100

    update dummy test

--- test/DumpingBandits.t.sol
+++ test/DumpingBandits.t.sol
@@ -2,19 +2,22 @@
 pragma solidity ^0.8.18;
 
 import "forge-std/Test.sol";
-import "../src/DumpingBandits.sol";
-import "../src/RandomnessClient.sol";
+import {RandomnessClient} from "../src/RandomnessClient.sol";
+import {DumpingBandits} from "../src/DumpingBandits.sol";
+import {BanditTreasury} from "../src/BanditTreasury.sol";
 
 contract DumpingBanditsTest is Test {
     RandomnessClient public rc;
-    DumpingBandits public db;
+    BanditTreasury public treasury;
+    DumpingBandits public dumpingBandits;
 
     function setUp() public {
         rc = new RandomnessClient();
-        db = new DumpingBandits(address(rc));
+        treasury = new BanditTreasury();
+        dumpingBandits = new DumpingBandits(address(rc), address(treasury));
     }
 
-    function testDummy() public {
+    function testDummy() public pure {
         assert(true);
     }
 }
